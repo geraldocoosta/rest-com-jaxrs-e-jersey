35cb88c45170ba318c1cde12c4152fb48a7dc29d
José Geraldo Costa Oliveira <e539740@mail.caixa>
2019-01-31 13:19:47 -0200

    termino da aula 1, projeto apos fazer o exercicio proposto

---
+++ src/main/java/br/com/alura/loja/dao/ProjetoDAO.java
@@ -0,0 +1,35 @@
+package br.com.alura.loja.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Projeto;
+
+public class ProjetoDAO {
+
+	private static Map<Long, Projeto> banco = new HashMap<>();
+	private static AtomicLong contador = new AtomicLong(2);
+
+	static {
+		banco.put(1l, new Projeto("minha loja", 1l, 2014));
+		banco.put(2l, new Projeto("ultcode", 2l, 2014));
+	}
+
+	public void adiciona(Projeto projeto) {
+		long id = contador.incrementAndGet();
+		projeto.setId(id);
+		banco.put(id, projeto);
+	}
+
+	public Projeto busca(long id) {
+		return banco.get(id);
+	}
+
+	public Projeto remove(long id) {
+		return banco.remove(id);
+	}
+
+}
